2b046a9eb4df0a9556ca55ffaff958ee3e6317d7
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-12-21 11:17:30 +0530

    Update rough2.py

--- rough2.py
+++ rough2.py
@@ -6,7 +6,7 @@ genai.configure(api_key='AIzaSyC3B0Z0hg6cZ08Jh8WP4OgsJtdfNcw')
  
  
 model = genai.GenerativeModel('gemini-pro')  
-prompt = f"tell a poem" 
+prompt = f"tell a poem"  
         
 response = model.generate_content(prompt)  
 print(response.text) 
