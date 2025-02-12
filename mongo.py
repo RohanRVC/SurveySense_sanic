87ddef81ca4416c151764a4277863b929c1bb516
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-02-12 10:48:58 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-client = AsyncIOMotorClient("mongodb://localhost:27017")
+client = AsyncIOMotorClient("mongodb://localhost:27017") 
 db = client["test_database"]
 
 async def test_connection():
