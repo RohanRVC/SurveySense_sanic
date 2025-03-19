9bc74edad1685066f768aa3297f1bebeabbe7e3f
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-03-19 10:59:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sanic import Sanic
 from backend.app.routes import setup_routes
 from sanic import Sanic   
 from sanic_cors import CORS           
-           
+            
 app = Sanic("SurveyProcessor")        
 CORS(app)  # This will allow all cross-origin requests by default          
    
