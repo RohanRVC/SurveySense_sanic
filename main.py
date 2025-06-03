679dcbb8e83a4f73c61c842f0b61701419fc950c
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-06-03 07:53:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ CORS(app)  # This will allow all cross-origin requests by default
     
 # Load Routes    
 setup_routes(app) 
-   
+    
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=8000, debug=True) 
