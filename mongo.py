6ce6ebf8eed79814576597f4ba6244e6898f1077
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-06-14 08:09:06 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 client = AsyncIOMotorClient("mongodb://localhost:27017") 
 db = client["test_database"]
    
-async def test_connection():  
+async def test_connection():   
     try:             
         await db.command("ping")  # Test the connection          
         print("MongoDB connection is successful!")      
